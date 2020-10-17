02a8632174e655b7fc158b74d240c4ab9e8185d3
Veernag <veeranagu552@gmail.com>
2020-10-17 20:12:37 +0530

    Added screenshot by robot class method in NewTopics class

--- src/test/java/com/caseStudy01/tests/NewTopics.java
+++ src/test/java/com/caseStudy01/tests/NewTopics.java
@@ -1,7 +1,17 @@
 package com.caseStudy01.tests;
 
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,6 +20,25 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import com.caseStudy1.utils.ConfigFileReader;
 
 public class NewTopics {
+	public String getScreenShotPathUsingRobotClass(String testCaseName, WebDriver driver) {
+		BufferedImage image = null;
+		String destinationFile = System.getProperty("user.dir") + "\\Reports\\" + testCaseName + ".png";
+		try {
+			image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+		} catch (HeadlessException e1) {
+			e1.printStackTrace();
+		} catch (AWTException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			ImageIO.write(image, "png", new File(destinationFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return destinationFile;
+	}
+
 	public static void main(String[] args) {
 		ConfigFileReader config = new ConfigFileReader();
 		System.setProperty(config.getChromeDriverProperty(), config.getChromeDriverPath());
@@ -17,6 +46,7 @@ public class NewTopics {
 		// Create object of ChromeOptions class
 		ChromeOptions options = new ChromeOptions();
 
+		// chrome show notifications popup
 		Map<String, Object> prefs = new HashMap<String, Object>();
 
 		// Set the notification setting it will override the default setting
@@ -28,13 +58,12 @@ public class NewTopics {
 		// dasabling developer mode extention
 		options.addArguments("--disable-extensions");
 
-		// chrome show notifications popup
-
 		// Start the chrome session
 		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("http://www.google.com");
-		
+
 		driver.quit();
+
 	}
 }
